4081945355c58e1fa923b281bfd6b79788ddacee
idomusha <idomusha@soot.black>
2019-05-28 23:41:56 -0400

    create 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import Layout from '../components/Layout'
+import Banner from '../components/Banner'
+
+import styles from './404.module.scss'
 
 export default function notFound() {
-  return <Layout>not found</Layout>
+  return (
+    <Layout className="full-screen">
+      <header className={styles.error}>
+        <Banner title="oops it's a dead end">
+          <Link to="/" className="button out-white">Back to home page</Link>
+        </Banner>
+      </header>
+    </Layout>
+  )
 }
