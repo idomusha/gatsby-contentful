9ee2fd51936cb9abbfd604cb1848f7ffe82fa624
idomusha <idomusha@soot.black>
2019-05-30 04:50:39 -0400

    add gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,8 +5,27 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: `idomusha`,
+    siteUrl: `https://www.soot.black`,
+    description: `front-end developer`,
+    author: `idomusha`,
+    email: `idomusha@soot.black`
+  },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+        ignore: [`**/\.*`], // ignore files starting with a dot
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+
     `gatsby-plugin-sass`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-transition-link`
   ],
 }
