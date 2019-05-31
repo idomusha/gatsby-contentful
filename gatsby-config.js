eff0228c08655ae1e8fcac1e8788406aeab01f55
idomusha <idomusha@soot.black>
2019-05-31 20:35:31 +0100

    add gatsby-plugin-react-css-modules

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-playground`,
+
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -30,6 +31,7 @@ module.exports = {
     },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+
     {
       resolve: `gatsby-source-contentful`,
       options: {
@@ -39,7 +41,24 @@ module.exports = {
     },
 
     `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-react-css-modules`,
+      options: {
+        // *.css files are included by default.
+        // To support another syntax (e.g. SCSS),
+        // add `postcss-scss` to your project's devDependencies
+        // and add the following option here:
+        filetypes: {
+          ".scss": { syntax: `postcss-scss` },
+        },
+
+        // Exclude global styles from the plugin using a RegExp:
+        exclude: `\/global\/`,
+        // For all the options check babel-plugin-react-css-modules README link provided above
+      },
+    },
     `gatsby-plugin-styled-components`,
+
     `gatsby-plugin-transition-link`,
   ],
 }
