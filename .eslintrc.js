30e6014c07286ffdbb7b79e67b0dfdf306d61f0f
idomusha <idomusha@soot.black>
2019-06-05 16:57:56 +0100

    update linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,29 @@ module.exports = {
   globals: {
     __PATH_PREFIX__: true,
   },
+  env: {
+    "browser": true,
+    "jest": true
+},
   extends: [
-    'react-app',
-    'airbnb',
-    'prettier',
-    'prettier/react'
+    "react-app",
+    "airbnb",
+    "prettier",
+    "prettier/react"
   ],
+  plugins: [
+    "prettier",
+    "react",
+  ],
+  rule: {
+    "max-len": ["error", {"code": 100}],
+
+    "prefer-promise-reject-errors": ["off"],
+
+    "react/jsx-filename-extension": ["off"],
+
+    "react/prop-types": ["warn"],
+
+    "no-return-assign": ["off"]
+  }
 };
